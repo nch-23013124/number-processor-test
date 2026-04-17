0df21b0531f42ad9f7c2831522d336b28c14b8d6
nch-23013124 <23013124@st.phenikaa-uni.edu.vn>
2026-04-17 07:29:07 +0700

    Add JUnit test for statement coverage (fix #1)

--- NumberProcessorTest.java
+++ NumberProcessorTest.java
@@ -5,25 +5,7 @@ public class NumberProcessorTest {
 
     @Test
     void testMixed() {
-        int[] arr = {1, 2};
-        assertEquals(1, NumberProcessor.processNumbers(arr));
-    }
-    @Test
-    void testAllEven() {
-    int[] arr = {2, 4};
-    assertEquals(6, NumberProcessor.processNumbers(arr));
-    }
-
-    @Test
-    void testAllOdd() {
-    int[] arr = {1, 3};
-    assertEquals(-4, NumberProcessor.processNumbers(arr));
-    }
-
-    @Test
-    void testEmpty() {
-    int[] arr = {};
-    assertEquals(0, NumberProcessor.processNumbers(arr));
-    }
-    
+        int[] arr = {2, 3};
+        assertEquals(-1, NumberProcessor.processNumbers(arr));
+    }    
 }
