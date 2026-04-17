c56ef4b18ff3296b325616dca39a0844d554847c
nch-23013124 <23013124@st.phenikaa-uni.edu.vn>
2026-04-17 07:08:18 +0700

    Add NumberProcessor with loop and branch

--- NumberProcessor.java
+++ NumberProcessor.java
@@ -1,1 +1,21 @@
+public class NumberProcessor {
 
+    public static int processNumbers(int[] arr) {
+        int result = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                result += arr[i];
+            } else {
+                result -= arr[i];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        System.out.println(processNumbers(arr));
+    }
+}
